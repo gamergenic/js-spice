8369ef5ef0589ba956dafef47d9767a27849a40f
gamergenic <82328213+gamergenic@users.noreply.github.com>
2023-12-27 14:06:43 -0800

    Added explain error messaages

--- src/source/utility/err.cpp
+++ src/source/utility/err.cpp
@@ -16,12 +16,15 @@ bool ErrorCheck(Napi::Env env)
         char szBuffer[SpiceLongMessageMaxLength];
 
         szBuffer[0] = '\0';
-        getmsg_c("LONG", sizeof(szBuffer), szBuffer);
+        getmsg_c("EXPLAIN", sizeof(szBuffer), szBuffer);
 
         if (!strnlen(szBuffer, sizeof(szBuffer)))
         {
-            szBuffer[0] = '\0';
-            getmsg_c("SHORT", sizeof(szBuffer), szBuffer);
+            getmsg_c("LONG", sizeof(szBuffer), szBuffer);
+            if (!strnlen(szBuffer, sizeof(szBuffer)))
+            {
+                getmsg_c("SHORT", sizeof(szBuffer), szBuffer);
+            }
         }
 
         Napi::Error::New(env, szBuffer).ThrowAsJavaScriptException();
