0cd8111804bf7342e7dcc8c865d3a7fd2d2294ed
gamergenic <82328213+gamergenic@users.noreply.github.com>
2023-12-28 17:57:43 -0800

    added tests for b1900, b1950

---
+++ test/b1900.js
@@ -0,0 +1,17 @@
+const expect = require('chai').expect;
+const { spice } = require('..');
+const expectAlmostEqual = require('./utility/expectAlmostEqual');
+
+describe('b1900', function() {
+  it('should return the Julian Date corresponding to Besselian Date 1900.0.',
+   function() {
+
+    // From:
+    // https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/C/cspice/b1900_c.html
+    const actual = spice.b1900();
+
+    const expected = 2415020.31352000;
+    const tolerance = 1e-4;
+    expect(actual).to.be.closeTo(expected, tolerance); 
+  });
+});
---
+++ test/b1950.js
@@ -0,0 +1,17 @@
+const expect = require('chai').expect;
+const { spice } = require('..');
+const expectAlmostEqual = require('./utility/expectAlmostEqual');
+
+describe('b1950', function() {
+  it('should return the Julian Date corresponding to Besselian Date 1950.0.',
+   function() {
+
+    // From:
+    // https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/C/cspice/b1900_c.html
+    const actual = spice.b1950();
+
+    const expected = 2433282.42345905;
+    const tolerance = 1e-4;
+    expect(actual).to.be.closeTo(expected, tolerance); 
+  });
+});
