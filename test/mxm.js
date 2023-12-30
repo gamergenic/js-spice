b88dfa5c2bc1ac1fe512c565799d9a2cf4cfefd9
gamergenic <82328213+gamergenic@users.noreply.github.com>
2023-12-29 17:16:41 -0800

    added test for mxm

--- test/mxm.js
+++ test/mxm.js
@@ -2,14 +2,15 @@ const expect = require('chai').expect;
 const { spice } = require('..');
 const expectAlmostEqual = require('./utility/expectAlmostEqual');
 
-describe('mtxm', function() {
-  it('matrix times identity should equal transpose', function() {
+describe('mxm', function() {
+  it('should multiply two 3x3 matrices', function() {
     let m1 = [[1, 2, 3], [4,5,6], [7,8,9]];
     let m2 = [[1, 0, 0], [0,1,0], [0,0,1]];
 
-    let actual = spice.mtxm(m1, m2);
+    let actual = spice.mxm(m1, m2);
 
-    let expected = [[1, 4, 7], [2,5,8], [3,6,9]];
+    let expected = [[1, 2, 3], [4,5,6], [7,8,9]];
+
     const tolerance = 1e-8;
     expectAlmostEqual(actual, expected, tolerance);
   });
